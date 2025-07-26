6a12716da70714590008af726e2566157369ab87
realmah06 <mahadeeabubakarmujaheed@gmail.com>
2025-07-27 00:18:54 +0100

    M

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,5 @@
- document.getElementById("footer").innerHTML = `
-            <p>&copy; 2023 A.A Mujaheed Global Enterprises. All rights reserved.</p>
-            <p>Developed by: <a href="https://wa.me/+2347046245050">Mahadi Abubakar Mujahid</a></p>
-        `;
+ let footer = document.getElementById("footer").innerHTML = `
+            <p>&copy; 2023 A.A Mujaheed Global Enterprises. All rights reserved.</p><p>Developed by: <a href="https://wa.me/+2347046245050">Mahadi Abubakar Mujahid</a></p>`;
 
         document.getElementById("loginForm").addEventListener('submit', function(event) {
             event.preventDefault();
