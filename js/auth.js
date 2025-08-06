a493cdb550217b09d31c6b743c089d8e3d54d06a
realmah06 <mahadeeabubakarmujaheed@gmail.com>
2025-08-06 01:43:35 +0100

    M

--- js/auth.js
+++ js/auth.js
@@ -1,9 +1,19 @@
-document.getElementById("login-space").innerHTML = ``;
-
    document.getElementById("loginForm").addEventListener('submit', function(event){
     event.preventDefault();
         var username = document.getElementById('username').value.trim();
         var password = document.getElementById('password').value.trim();
 
+        if(username === ''){
+            Swal.fire({
+                icon: 'warning',
+                html: `<p class="title-alert">Missing Field!</p>`,
+                html: `<p class="alert-message">Sorry!, Fill in the Username Field</p>`,
+                customClass: {
+                    confirmButton: 'btn btn-success',
+                    cancelButton: 'btn btn-danger',
+                    denyButton: 'btn btn-secondary'
+                }
+            })
+        }
        
    })
