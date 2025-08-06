aa6c15205a235252bed5489201759cd2294fef9a
realmah06 <mahadeeabubakarmujaheed@gmail.com>
2025-08-06 01:31:39 +0100

    M

--- js/auth.js
+++ js/auth.js
@@ -1,26 +1,9 @@
-document.getElementById("login-space").innerHTML = `<div class="login-form">
-        <center><img src="images/logo.jpg" alt="logo">
-        <h1>A A Mujaheed Global Enterprises</h1>
-        <!--<small><em>Specialized on Sales & Supply of: Maize, Fertilizer, Soya Beans, Agro-Chemicals, etc.</em></small>-->
-        <hr style="border: 1px solid darkgreen;">
-        </center>
-        <br>
-        <form id="loginForm">
-            <div class="form-group">
-                <label for="username">Username</label>
-                <input type="text" name="username" id="username" placeholder="Username">
-            </div>
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" name="password" id="password" placeholder="Password">
-            </div>
-            <input type="checkbox"> <span>Keep me logged in</span> <a class="forgot" href="#">Reset my Password?</a>
-            <br>
-            <div class="form-group">
-                <center>
-                    <button type="submit">Sign In</button>
-                </center>
-            </div>
-            <br><br><br>
-        </form>
-   </div>`
+document.getElementById("login-space").innerHTML = ``;
+
+   document.getElementById("loginForm").addEventListener('submit', function(event){
+    event.preventDefault();
+        var username = document.getElementById('username').value.trim();
+        var password = document.getElementById('password').value.trim();
+
+       
+   })
